ec9147b0dd804d6ec619bf561859b1bf8d9e43e7
Louis Orleans <louis@orleans.io>
2023-06-13 20:29:49 -0700

    🐛 fix cwd check

--- functions/_node_version_on_prompt.fish
+++ functions/_node_version_on_prompt.fish
@@ -1,5 +1,5 @@
 function _node_version_on_prompt --on-event fish_prompt
-  if "$_node_version_old_cwd" != "(pwd)"
+  if [ "$_node_version_old_cwd" != "$(pwd)" ]
     set --global _node_version_old_cwd (pwd)
     emit _node_version_cwd
   end
